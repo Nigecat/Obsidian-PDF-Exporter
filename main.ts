d12aeb4b695fa0eba1036c124d2d619eedce8d33
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-11-04 15:42:04 +1100

    Add sidebar button

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 import { Plugin } from "obsidian";
 
 export default class PDFExporter extends Plugin {
-
+    onload() {
+        this.addRibbonIcon("dice", "Export PDF", () => {
+            console.log("button clicked!");
+        });
+    }
 }
