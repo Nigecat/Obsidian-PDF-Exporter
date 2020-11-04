c25b958fb16534335b3c56ee12f754eab1f6d407
Nigecat <48661288+Nigecat@users.noreply.github.com>
2020-11-04 14:47:44 +1100

    Setup environment

---
+++ main.ts
@@ -0,0 +1,5 @@
+import { Plugin } from "obsidian";
+
+export default class PDFExporter extends Plugin {
+
+}
---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+
+export default {
+    input: "main.ts",
+    output: {
+        dir: ".",
+        sourcemap: "inline",
+        format: "cjs",
+        exports: "default"
+    },
+    external: ["obsidian"],
+    plugins: [
+        typescript(),
+        nodeResolve({ browser: true }),
+        commonjs(),
+    ]
+}
